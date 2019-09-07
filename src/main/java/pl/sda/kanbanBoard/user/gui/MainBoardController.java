e7e39f651f1b226d248b17ded1dcf86bd4e0a8bb
Adrian <abrymut@gmail.com>
2019-09-07 13:41:05 +0200

    Merge branch 'masterSettingAdrian' of /home/adrian/IdeaProjects/7.09 Kanban/kanban with conflicts.

--- src/main/java/pl/sda/kanbanBoard/user/gui/MainBoardController.java
+++ src/main/java/pl/sda/kanbanBoard/user/gui/MainBoardController.java
@@ -1,12 +1,25 @@
 package pl.sda.kanbanBoard.user.gui;
-
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import pl.sda.kanbanBoard.user.api.ServerWriter;
+import pl.sda.kanbanBoard.user.api.ServerHandler;
+
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+
 
 public class MainBoardController {
+
+    MainBoardController mainBoardController;
+    PrintWriter writer;
+    Socket socket;
+    BufferedReader reader;
 
     @FXML
     private Label label;
@@ -17,22 +30,22 @@ public class MainBoardController {
     @FXML
     private Button createTaskButton;
 
-    private ServerWriter serverWriter;
+    ServerHandler serverHandler = new ServerHandler(writer, socket, reader, message, label);
 
-      @FXML
+    @FXML
     public void initialize() {
+        serverHandler.configureCommunication();
+        //MainBoardController mainBoardController = this.mainBoardController;
+        Thread recieverThread = new Thread(serverHandler.getReciever());
+        recieverThread.start();
     }
 
     public void createTask() {
-        serverWriter.createTask(message.getText());
-    }
-
-    public void taskCreated(String newLabel) {
-          label.setText(newLabel);
+        serverHandler.createTask();
     }
 
 
-    public void setServerWriter(ServerWriter serverWriter) {
-        this.serverWriter = serverWriter;
-    }
+
+
+
 }
