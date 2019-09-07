e584e5789ff2a67f204352d56c7f669f95a8fc99
hausdorffspace <rafal.antas96@gmail.com>
2019-09-08 00:47:58 +0200

    do some cosmetic thinks

--- src/main/java/pl/sda/kanbanBoard/server/BasicServer.java
+++ src/main/java/pl/sda/kanbanBoard/server/BasicServer.java
@@ -1,6 +1,7 @@
 package pl.sda.kanbanBoard.server;
 
 import pl.sda.kanbanBoard.server.task_Repository.TaskRepositoryImplementation;
+import pl.sda.kanbanBoard.server.task_Repository.TaskRepositoryInterface;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -13,17 +14,15 @@ import java.util.Iterator;
 public class BasicServer {
     ArrayList outputStreams;
 
-    public  class ClientHandler implements Runnable {
+    public class ClientHandler implements Runnable {
         BufferedReader reader;
         Socket socket;
 
         public ClientHandler(Socket clientSocket) {
-
             try {
                 socket = clientSocket;
                 InputStreamReader isReader = new InputStreamReader(socket.getInputStream());
                 reader = new BufferedReader(isReader);
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -37,7 +36,7 @@ public class BasicServer {
                 while ((message = reader.readLine()) != null) {
                     System.out.println("Readed" + message);
                     send(message);
-                    TaskRepositoryImplementation taskRepositoryImplementation = new TaskRepositoryImplementation();
+                    TaskRepositoryInterface taskRepositoryImplementation = new TaskRepositoryImplementation();
                     taskRepositoryImplementation.writeDataToFile(message);
                 }
             } catch (Exception e) {
@@ -46,38 +45,40 @@ public class BasicServer {
         }
     }
 
-    public void send(String message){
-            Iterator it = outputStreams.iterator();
-            while (it.hasNext()){
-                try{
-                    PrintWriter writer = (PrintWriter) it.next();
-                    writer.println(message);
-                    writer.flush();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
+    public void send(String message) {
+        Iterator it = outputStreams.iterator();
+        while (it.hasNext()) {
+            try {
+                PrintWriter writer = (PrintWriter) it.next();
+                writer.println(message);
+                writer.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+        }
 
     }
-    public void start(){
+
+    public void start() {
         outputStreams = new ArrayList();
         try {
             ServerSocket serverSocket = new ServerSocket(5000);
-            while(true){
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
                 PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
                 outputStreams.add(writer);
-                Thread t = new Thread(new ClientHandler(clientSocket));
-                t.start();
+                Thread thread = new Thread(new ClientHandler(clientSocket));
+                thread.start();
                 System.out.println("We have connection");
             }
-
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
+    //TODO
+    //mylna metoda z klasy Thread,  zmienic nazwe
     public static void main(String[] args) {
         new BasicServer().start();
     }
