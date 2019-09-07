19dff3463b4de3aadc3f507292a5203c06134c4b
hausdorffspace <rafal.antas96@gmail.com>
2019-09-07 11:58:28 +0200

    add method that save to file

--- src/main/java/pl/sda/kanbanBoard/server/BasicServer.java
+++ src/main/java/pl/sda/kanbanBoard/server/BasicServer.java
@@ -1,8 +1,6 @@
 package pl.sda.kanbanBoard.server;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -42,6 +40,20 @@ public class BasicServer {
             }
         }
     }
+
+    public void saveToFile(String message) throws IOException {
+        BufferedWriter saveToFileObject = null;
+        try {
+            saveToFileObject = new BufferedWriter(new FileWriter("baseData.txt"));
+            saveToFileObject.write(message);
+            saveToFileObject.newLine();
+        } catch (Exception e) {
+            e.getStackTrace();
+        } finally {
+            saveToFileObject.close();
+        }
+    }
+
     public void send(String message){
             Iterator it = outputStreams.iterator();
             while (it.hasNext()){
