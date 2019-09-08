40fa5ad04e881b4d479d52c6ffc7f95b8b17bfc8
Adrian <abrymut@gmail.com>
2019-09-08 15:00:16 +0200

    no problemaitc spaces

--- src/main/java/pl/sda/kanbanBoard/server/task_Repository/TaskRepositoryInterface.java
+++ src/main/java/pl/sda/kanbanBoard/server/task_Repository/TaskRepositoryInterface.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public interface TaskRepositoryInterface {
-    boolean writeDataToFile(String messag, Integer id) throws IOException;
+    boolean writeDataToFile(String message, Integer id) throws IOException;
     void deleteDataFromFile();
     String takeDataFromFile();
 }
